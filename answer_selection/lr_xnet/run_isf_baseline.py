8db8dc4ab2d9a49af6523742ce9580aa22e12c8e
ronaldahmed <roncardenasacosta@gmail.com>
2018-11-10 15:45:32 +0100

    consistent metrics setup

--- answer_selection/lr_xnet/run_isf_baseline.py
+++ answer_selection/lr_xnet/run_isf_baseline.py
@@ -7,6 +7,7 @@ import pdb
 import sys
 from collections import defaultdict
 from utils_extra import *
+import argparse
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__)) # ./
 
@@ -31,11 +32,13 @@ def reformat_dataset(labels,sc_data):
 
 
 if __name__ == "__main__":
-  if len(sys.argv) != 2:
-    print("Usage: python run_isf_baseline.py <dataset>")
-    sys.exit(1)
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--dataset", '-d', default="wikiqa", type=str, help="Dataset name")
+  parser.add_argument("--mode", '-m', default="first", type=str, help="tie-breaking strategy [first,last]")
+  args = parser.parse_args()
 
-  dataset = sys.argv[1]
+  dataset = args.dataset
+
   PREPROC_DATA_DIR = os.path.join("..",'datasets','preprocessed_data',dataset)
 
   splits = ['training','validation','test']
@@ -51,7 +54,9 @@ if __name__ == "__main__":
     idf_scores_data_list = open("%s/%s.idf.scores" % (PREPROC_DATA_DIR,corpus_split),'r').read().strip().split("\n\n") # 
     locisf_scores_data_list = open("%s/%s.locisf.scores" % (PREPROC_DATA_DIR,corpus_split),'r').read().strip().split("\n\n") # 
     cnt_scores_data_list    = open("%s/%s.cnt.scores" % (PREPROC_DATA_DIR,corpus_split),'r').read().strip().split("\n\n") # 
-    #wgt_scores_data_list    = open("%s/%s.wgtcnt.scores" % (PREPROC_DATA_DIR,corpus_split),'r').read().strip().split("\n\n") # 
+    if dataset=="wikiqa":
+      wgt_scores_data_list    = open("%s/%s.wgtcnt.scores" % (PREPROC_DATA_DIR,corpus_split),'r').read().strip().split("\n\n") # 
+  
   
     ndocs = len(label_data_list)
     labels_data = []
@@ -67,7 +72,8 @@ if __name__ == "__main__":
       idf_lines = idf_scores_data_list[doc_idx].strip().split("\n")
       locisf_lines = locisf_scores_data_list[doc_idx].strip().split("\n")
       cnt_lines = cnt_scores_data_list[doc_idx].strip().split("\n")
-      #wgt_lines = wgt_scores_data_list[doc_idx].strip().split("\n")
+      if dataset=="wikiqa":
+        wgt_lines = wgt_scores_data_list[doc_idx].strip().split("\n")
 
       labels = np.array([int(x) for x in label_lines[1:]])
       labels = (labels>0).astype(float) # newsqa has 0,1,2
@@ -75,44 +81,58 @@ if __name__ == "__main__":
       idf_scores = np.array([float(x) for x in idf_lines[1:]])
       locisf_scores = np.array([float(x) for x in locisf_lines[1:]])
       cnt_scores = np.array([float(int(x)) for x in cnt_lines[1:]])
-      #wgt_scores = np.array([float(x) for x in wgt_lines[1:]])
+      if dataset=="wikiqa":
+        wgt_scores = np.array([float(x) for x in wgt_lines[1:]])
       
       labels_data.append(labels)
       isf_data.append(isf_scores)
       idf_data.append(idf_scores)
       locisf_data.append(locisf_scores)
       cnt_data.append(cnt_scores)
-      #wgt_data.append(wgt_scores)
+      if dataset=="wikiqa":
+        wgt_data.append(wgt_scores)
     ##
 
     ###
-    print(corpus_split)
-    print("Total instances: %d" % ndocs)
-    pred,y,dls = reformat_dataset(labels_data,isf_data)
-    acc = accuracy(y,pred,dls)
-    mrr = mrr_metric(y,pred,dls,corpus_split)
-    _map = map_score(y,pred,dls,corpus_split)
-    print("ISF baseline    : ACC:%.4f | MAP:%.4f | MRR:%.4f" % (acc,_map,mrr) )
+    pred,y,dls = reformat_dataset(labels_data,cnt_data)
+    acc = accuracy(y,pred,dls,args.mode)
+    mrr = mrr_metric(y,pred,dls,corpus_split,args.mode)
+    _map = map_score(y,pred,dls,corpus_split,args.mode)
+    print("Cnt baseline    : ACC:%.4f | MAP:%.4f | MRR:%.4f" % (acc,_map,mrr) )
+    # print("Cnt baseline,%.4f,%.4f,%.4f" % (acc,_map,mrr) )
+
+    
+    if dataset=="wikiqa":
+      pred,y,dls = reformat_dataset(labels_data,wgt_data)
+      acc = accuracy(y,pred,dls,args.mode)
+      mrr = mrr_metric(y,pred,dls,corpus_split,args.mode)
+      _map = map_score(y,pred,dls,corpus_split,args.mode)
+      print("Wgt Cnt baseline: ACC:%.4f | MAP:%.4f | MRR:%.4f" % (acc,_map,mrr) )
+      # print("Wgt Cnt baseline,%.4f,%.4f,%.4f" % (acc,_map,mrr) )
+    
+    else:
+      pred,y,dls = reformat_dataset(labels_data,idf_data)
+      acc = accuracy(y,pred,dls,args.mode)
+      mrr = mrr_metric(y,pred,dls,corpus_split,args.mode)
+      _map = map_score(y,pred,dls,corpus_split,args.mode)
+      print("IDF baseline    : ACC:%.4f | MAP:%.4f | MRR:%.4f" % (acc,_map,mrr) )
+      # print("IDF baseline,%.4f,%.4f,%.4f" % (acc,_map,mrr) )
+
     
     pred,y,dls = reformat_dataset(labels_data,locisf_data)
-    acc = accuracy(y,pred,dls)
-    mrr = mrr_metric(y,pred,dls,corpus_split)
-    _map = map_score(y,pred,dls,corpus_split)
+    acc = accuracy(y,pred,dls,args.mode)
+    mrr = mrr_metric(y,pred,dls,corpus_split,args.mode)
+    _map = map_score(y,pred,dls,corpus_split,args.mode)
     print("Loc-ISF baseline: ACC:%.4f | MAP:%.4f | MRR:%.4f" % (acc,_map,mrr) )
-
+    # print("Loc-ISF baseline,%.4f,%.4f,%.4f" % (acc,_map,mrr) )
     
-    pred,y,dls = reformat_dataset(labels_data,cnt_data)
-    acc = accuracy(y,pred,dls)
-    mrr = mrr_metric(y,pred,dls,corpus_split)
-    _map = map_score(y,pred,dls,corpus_split)
-    print("Cnt baseline    : ACC:%.4f | MAP:%.4f | MRR:%.4f" % (acc,_map,mrr) )
-    
-
-    pred,y,dls = reformat_dataset(labels_data,idf_data)
-    acc = accuracy(y,pred,dls)
-    mrr = mrr_metric(y,pred,dls,corpus_split)
-    _map = map_score(y,pred,dls,corpus_split)
-    print("IDF baseline    : ACC:%.4f | MAP:%.4f | MRR:%.4f" % (acc,_map,mrr) )
+      
+    pred,y,dls = reformat_dataset(labels_data,isf_data)
+    acc = accuracy(y,pred,dls,args.mode)
+    mrr = mrr_metric(y,pred,dls,corpus_split,args.mode)
+    _map = map_score(y,pred,dls,corpus_split,args.mode)
+    print("ISF baseline    : ACC:%.4f | MAP:%.4f | MRR:%.4f" % (acc,_map,mrr) )
+    # print("ISF baseline,%.4f,%.4f,%.4f" % (acc,_map,mrr) )
     
 
     print
--- answer_selection/lr_xnet/utils_extra.py
+++ answer_selection/lr_xnet/utils_extra.py
@@ -36,26 +36,43 @@ def dump_trec_format(labels,scores,doc_lens):
     output.close()
 
 
-def accuracy(labels,one_probs,doc_lens):
+def accuracy(labels,one_probs,doc_lens,mode="first"):
     idx = 0
     correct = 0
     total = 0
     for dl in doc_lens:
         prob = one_probs[idx:idx+dl]
         label = labels[idx:idx+dl]
-        sum_label = sum(label)
-        if sum_label==0 or sum_label==dl:
+        if sum(label)==0:
             idx += dl
             continue
-        off = prob.argmax()
-        correct += labels[idx+off]
+
+        if mode=="first":
+            off = prob.argmax()
+            correct += labels[idx+off]
+        else:
+            srt_ref = [(x,i) for i,x in enumerate(prob)]
+            srt_ref.sort(reverse=True)
+            correct += label[srt_ref[0][1]]
+
         idx += dl
         total += 1.0
     acc = 1.0 * correct / total
     return acc
 
 
-def mrr_metric(labels,one_probs,doc_lens,data_type="training"):
+def mrr_metric(labels,one_probs,doc_lens,data_type="training",mode="first"):
+    return mrr_metric_local(labels,one_probs,doc_lens,data_type,mode)
+
+
+def map_score(labels,one_probs,doc_lens,data_type="training",mode="first"):
+    return map_score_local(labels,one_probs,doc_lens,data_type,mode)
+
+
+
+###################################################################
+
+def mrr_metric_trec(labels,one_probs,doc_lens,data_type="training"):
     dump_trec_format(labels,one_probs,doc_lens)
     popen = sp.Popen(["../trec_eval/trec_eval",
                 "-m", "recip_rank",
@@ -68,7 +85,43 @@ def mrr_metric(labels,one_probs,doc_lens,data_type="training"):
     return mrr
 
 
-def map_score(labels,one_probs,doc_lens,data_type="training"):
+def mrr_metric_local(labels,one_probs,doc_lens,data_type="training",mode="first"):
+    idx = 0
+    mrr = 0.0
+    total = 0.0
+    for dl in doc_lens:
+        probs = one_probs[idx:idx+dl].reshape([-1])
+        label = labels[idx:idx+dl]
+        if sum(label)==0:
+            idx += dl
+            continue
+
+        srt_ref = []
+        # tie breaking: earliest in list
+        if mode=="first":
+            srt_ref = [(-x,i) for i,x in enumerate(probs)]
+            srt_ref.sort()
+        # tie breaking: last in list
+        else:
+            srt_ref = [(x,i) for i,x in enumerate(probs)]
+            srt_ref.sort(reverse=True)
+
+        rel_rank = 0.0
+        for idx_retr,(x,i) in enumerate(srt_ref):
+            if label[i]==1:
+                rel_rank = 1.0 + idx_retr
+                break
+
+        idx += dl
+        mrr += 1.0/rel_rank # accumulate inverse rank
+        total += 1.0
+    mrr /= total
+    return mrr
+
+
+###################################################################
+
+def map_score_trec(labels,one_probs,doc_lens,data_type="training",mode=""):
     dump_trec_format(labels,one_probs,doc_lens)
     popen = sp.Popen(["../trec_eval/trec_eval",
                 "-m", "map",
@@ -79,6 +132,45 @@ def map_score(labels,one_probs,doc_lens,data_type="training"):
         metric = f.read().strip("\n")[-6:]
         map_sc = float(metric)
     return map_sc
+
+
+def map_score_local(labels,one_probs,doc_lens,data_type="training",mode="first"):
+    idx = 0
+    _map = 0.0
+    total = 0.0
+    for dl in doc_lens:
+        probs = one_probs[idx:idx+dl].reshape([-1])
+        label = labels[idx:idx+dl]
+        if sum(label)==0:
+            idx += dl
+            continue
+        srt_ref = []
+        # tie breaking: earliest in list
+        if mode=="first":
+            srt_ref = [(-x,i) for i,x in enumerate(probs)]
+            srt_ref.sort()
+        # tie breaking: last in list
+        else:
+            srt_ref = [(x,i) for i,x in enumerate(probs)]
+            srt_ref.sort(reverse=True)
+
+        aps = 0.0
+        n_corr = 0.0
+        for idx_retr,(x,i) in enumerate(srt_ref):
+            if label[i]==1:
+                n_corr += 1.0
+                aps += (n_corr / (idx_retr+1))
+                # break
+        aps /= sum(label)
+
+        idx += dl
+        total += 1.0
+        _map += aps
+    _map /= total
+    return _map
+
+
+###################################################################
 
 
 def load_trec_prediction(filename):
@@ -169,6 +261,4 @@ def get_sentence_lvl_dataset(probs,batch,wang_preds,mode,ign_indexes):
     Y = np.array(Y,dtype=np.float32)
 
     return X,Y,filt_doc_len
-
-
 
